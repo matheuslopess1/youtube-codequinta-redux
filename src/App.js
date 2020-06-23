1067e323ce4e2b15e098fdb08f1f71705a40824d
Matheus Inocencio Lopes <matheuslopes1@protonmail.com>
2020-06-23 14:22:28 -0300

    Live finalizada. Impl. e Doc.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+import TodoList from "./TodoList";
+import Counter from "./Counter";
 
 function App() {
-  return <div>Hey</div>;
+  return (
+    <Provider store={store}>
+      <TodoList />
+      <Counter />
+    </Provider>
+  );
 }
 
 export default App;
